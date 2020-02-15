1616eae3f0fc3a066af9ba5665d1a28408bac70b
Alexandre Ronsaut <alex@apollonet.fr>
2020-02-15 15:30:08 +0100

    Rename config test file

--- test/set-config.js
+++ test/set-config.js
@@ -3,7 +3,7 @@
 const test = require('ava')
 const setConfig = require('../lib/set-config.js')
 
-test('Validate config', t => {
+test('Set config', t => {
   const config = setConfig(__dirname)
   t.truthy(config.content)
 })
