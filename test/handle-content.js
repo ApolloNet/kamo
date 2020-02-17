80b53f0779fd1e975c9f90bd954f77f575c83c0b
Alexandre Ronsaut <alex@apollonet.fr>
2020-02-17 13:25:10 +0100

    Fix tests for handle dirs

--- test/handle-content.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const test = require('ava')
-const handleContent = require('../lib/handle-content.js')
-
-test('Handle content', async t => {
-  const files = await handleContent('./test/mock')
-  t.true(Array.isArray(files))
-})
