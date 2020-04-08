b85458ae966722cd7a82d28c286aceeb59601c40
Alexandre Ronsaut <alex@apollonet.fr>
2020-04-08 15:36:48 +0200

    Fix dir handling test

--- test/handle-dirs.js
+++ test/handle-dirs.js
@@ -3,5 +3,5 @@ const handleDirs = require('../lib/handle-dirs.js')
 
 test('Handle dirs', async t => {
   const files = await handleDirs()
-  t.deepEqual(files[0], 'test/mock/birdman.md')
+  t.truthy(files.includes('test/mock/birdman.md'))
 })
