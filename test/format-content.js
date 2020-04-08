a8881ca3cff045c1c2bbb3eee59e8ba154b61df5
Alexandre Ronsaut <alex@apollonet.fr>
2020-04-08 15:42:44 +0200

    Remove geo format tests

--- test/format-content.js
+++ test/format-content.js
@@ -6,6 +6,5 @@ test('Format content', async t => {
   const content = await formatContent(filePath)
   t.deepEqual(content.name, 'the-ugly-duckling')
   t.deepEqual(content.title, 'The Ugly Duckling')
-  t.truthy(content.geo[0].lat)
   t.deepEqual(content.dates[0].start.short, '13 septembre 2020')
 })
