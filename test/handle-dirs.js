d994d1786fd6eb4b528a21c92a291aeeff227991
Alexandre Ronsaut <alex@apollonet.fr>
2020-04-08 15:32:12 +0200

    Fix dirs handling

--- test/handle-dirs.js
+++ test/handle-dirs.js
@@ -1,14 +1,7 @@
 const test = require('ava')
 const handleDirs = require('../lib/handle-dirs.js')
 
-test('Handle one dir', async t => {
-  const dir = './test/mock'
-  const files = await handleDirs(dir)
-  t.true(Array.isArray(files))
-})
-
-test('Handle multiple dirs', async t => {
-  const dirs = ['./test/mock']
-  const files = await handleDirs(dirs)
-  t.true(Array.isArray(files))
+test('Handle dirs', async t => {
+  const files = await handleDirs()
+  t.deepEqual(files[0], 'test/mock/birdman.md')
 })
