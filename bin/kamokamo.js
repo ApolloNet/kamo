a35faa143e766b1980d36d5726c259af6bf420d6
Alexandre Ronsaut <alex@apollonet.fr>
2020-04-08 12:06:25 +0200

    Load config as an object

--- bin/kamokamo.js
+++ bin/kamokamo.js
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 
 const process = require('process')
-const setConfig = require('../lib/set-config')
 const handleDirs = require('../lib/handle-dirs')
 const formatContent = require('../lib/format-content')
 const formatJson = require('../lib/format-json')
 
 async function kamokamo() {
-  const config = setConfig()
-  const filePaths = await handleDirs(config.dirs)
+  const filePaths = await handleDirs()
   const contents = await Promise.all(
-    filePaths.map(filePath => formatContent(filePath, config))
+    filePaths.map(filePath => formatContent(filePath))
   )
   const json = formatJson(contents)
   process.stdout.write(json)
--- test/format-content.js
+++ test/format-content.js
@@ -1,11 +1,9 @@
 const test = require('ava')
-const setConfig = require('../lib/set-config.js')
 const formatContent = require('../lib/format-content.js')
 
 test('Format content', async t => {
   const filePath = './test/mock/the-ugly-duckling.md'
-  const config = setConfig(__dirname)
-  const content = await formatContent(filePath, config)
+  const content = await formatContent(filePath)
   t.deepEqual(content.name, 'the-ugly-duckling')
   t.deepEqual(content.title, 'The Ugly Duckling')
   t.deepEqual(content.geo.lat, '49.4381635')
